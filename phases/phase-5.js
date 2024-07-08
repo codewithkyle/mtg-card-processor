f86f8c1c83c16e679e743535169c4a169628fda3
Kyle Andrews <codingwithkyle@gmail.com>
2024-07-08 08:43:28 -0400

    Fixed upsert query issues

--- phases/phase-5.js
+++ phases/phase-5.js
@@ -28,21 +28,23 @@ function reportError(error, card){
 }
 
 let cards;
-const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+//const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 async function write(dir) {
     const data = await fs.promises.readFile(path.join(dir, "card.json"), { encoding: "utf-8" });
     const card = JSON.parse(data);
 
     const oldCard = await new Promise((resolve, reject) => {
-        connection.query(`SELECT HEX(id) as id FROM Cards WHERE id = UNHEX('${card.id}') AND oracle_id IS NULL`, (err, results) => {
+        connection.query(`SELECT name, HEX(id) as id, HEX(oracle_id) as oracleId FROM Cards WHERE id = UNHEX('${card.id}') AND oracle_id IS NULL`, (err, results) => {
             if (err) reject(err);
             resolve(results?.[0] ?? null);
         });
     });
-
     if (oldCard === null) {
+        console.log("New: ", card.name);
         card.id = uuidv4().replace(/-/g, "");
+    } else {
+        console.log("Found card: ", oldCard.name);
     }
 
     if (card.front !== null){
@@ -79,7 +81,7 @@ async function write(dir) {
 }
 
 function purgeTables(card){
-    const tables = ["Card_Subtypes", "Card_Keywords", "Card_Flavor_Text", "Card_Text", "Card_Names", "Card_Colors"];
+    const tables = ["Card_Subtypes", "Card_Keywords", "Card_Flavor_Text", "Card_Texts", "Card_Names", "Card_Colors"];
     for (const table of tables){
         connection.query(
             `DELETE FROM ${table} WHERE card_id = UNHEX(?)`,
@@ -167,7 +169,7 @@ function insertCardTexts(card){
         querySegments.push("(UNHEX(?), UNHEX(?), ?)");
     }
     connection.query(
-        `INSERT INTO Card_Text (id, card_id, text) VALUES ${querySegments.join(", ")}`,
+        `INSERT INTO Card_Texts (id, card_id, text) VALUES ${querySegments.join(", ")}`,
         values, 
         (error) => {
             if (error) {
@@ -220,6 +222,7 @@ function insertCardColors(card){
 }
 
 function updateCard(card){
+    console.log("Updating: ", card.name);
     try {
         card.power = parseInt(card.power);
         if (isNaN(card.power)){
@@ -236,9 +239,9 @@ function updateCard(card){
     } catch (error){
         card.toughness = null;
     }
-    const query = `UPDATE Cards SET oracle_id = UNHEX(?), layout = ?, front = ?, back = ?, art = ?, rarity = ?, type = ?, toughness = ?, power = ?, manaCost = ?, totalManaCost = ?, standard = ?, future = ?, historic = ?, gladiator = ?, pioneer = ?, explorer = ?, modern = ?, legacy = ?, pauper = ?, vintage = ?, penny = ?, commander = ?, oathbreaker = ?, brawl = ?, historicbrawl = ?, alchemy = ?, paupercommander = ?, duel = ?, oldschool = ?, premodern = ?, predh = ? WHERE id = UNHEX(?) AND oracle_id IS NULL`;
+    const query = `UPDATE Cards SET oracle_id = UNHEX(?), layout = ?, front = ?, back = ?, art = ?, rarity = ?, type = ?, toughness = ?, power = ?, manaCost = ?, totalManaCost = ?, standard = ?, future = ?, historic = ?, gladiator = ?, pioneer = ?, explorer = ?, modern = ?, legacy = ?, pauper = ?, vintage = ?, penny = ?, commander = ?, oathbreaker = ?, brawl = ?, historicbrawl = ?, alchemy = ?, paupercommander = ?, duel = ?, oldschool = ?, premodern = ?, predh = ? WHERE id = UNHEX(?)`;
     const params = [
-        card.oracle_id,
+        card.oracleId.replace(/-/g, ""),
         card.layout,
         card.front,
         card.back,
@@ -249,37 +252,39 @@ function updateCard(card){
         card.power,
         card.manaCost?.[0] ?? null,
         card.totalManaCost,
-        card.legalities.standard ? 1 : 0,
-        card.legalities.future ? 1 : 0,
-        card.legalities.historic ? 1 : 0,
-        card.legalities.gladiator ? 1 : 0,
-        card.legalities.pioneer ? 1 : 0,
-        card.legalities.explorer ? 1 : 0,
-        card.legalities.modern ? 1 : 0,
-        card.legalities.legacy ? 1 : 0,
-        card.legalities.pauper ? 1 : 0,
-        card.legalities.vintage ? 1 : 0,
-        card.legalities.penny ? 1 : 0,
-        card.legalities.commander ? 1 : 0,
-        card.legalities.oathbreaker ? 1 : 0,
-        card.legalities.brawl ? 1 : 0,
-        card.legalities.historicbrawl ? 1 : 0,
-        card.legalities.alchemy ? 1 : 0,
-        card.legalities.paupercommander ? 1 : 0,
-        card.legalities.duel ? 1 : 0,,
-        card.legalities.oldschool ? 1 : 0,
-        card.legalities.premodern ? 1 : 0,
-        card.legalities.predh ? 1 : 0,
+        card.legalities?.standard ? 1 : 0,
+        card.legalities?.future ? 1 : 0,
+        card.legalities?.historic ? 1 : 0,
+        card.legalities?.gladiator ? 1 : 0,
+        card.legalities?.pioneer ? 1 : 0,
+        card.legalities?.explorer ? 1 : 0,
+        card.legalities?.modern ? 1 : 0,
+        card.legalities?.legacy ? 1 : 0,
+        card.legalities?.pauper ? 1 : 0,
+        card.legalities?.vintage ? 1 : 0,
+        card.legalities?.penny ? 1 : 0,
+        card.legalities?.commander ? 1 : 0,
+        card.legalities?.oathbreaker ? 1 : 0,
+        card.legalities?.brawl ? 1 : 0,
+        card.legalities?.historicbrawl ? 1 : 0,
+        card.legalities?.alchemy ? 1 : 0,
+        card.legalities?.paupercommander ? 1 : 0,
+        card.legalities?.duel ? 1 : 0,
+        card.legalities?.oldschool ? 1 : 0,
+        card.legalities?.premodern ? 1 : 0,
+        card.legalities?.predh ? 1 : 0,
         card.id,
     ];
     connection.query(query, params, (error) => {
         if (error) {
+            console.log(error);
             reportError(error, card);
         }
     });
 }
 
 function insertCard(card){
+    console.log("Inserting: ", card.name);
     try {
         card.power = parseInt(card.power);
         if (isNaN(card.power)){
@@ -296,10 +301,11 @@ function insertCard(card){
     } catch (error){
         card.toughness = null;
     }
-    const query = `INSERT INTO Cards (id, oracle_id, layout, front, back, art, rarity, type, toughness, power, manaCost, totalManaCost, standard, future, historic, gladiator, pioneer, explorer, modern, legacy, pauper, vintage, penny, commander, oathbreaker, brawl, historicbrawl, alchemy, paupercommander, duel, oldschool, premodern, predh) VALUES (UNHEX(?), UNHEX(?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const query = `INSERT INTO Cards (id, oracle_id, name, layout, front, back, art, rarity, type, toughness, power, manaCost, totalManaCost, standard, future, historic, gladiator, pioneer, explorer, modern, legacy, pauper, vintage, penny, commander, oathbreaker, brawl, historicbrawl, alchemy, paupercommander, duel, oldschool, premodern, predh) VALUES (UNHEX(?), UNHEX(?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
     const params = [
         card.id,
-        card.oracle_id,
+        card.oracleId.replace(/-/g, ""),
+        card.name,
         card.layout,
         card.front,
         card.back,
@@ -327,7 +333,7 @@ function insertCard(card){
         card.legalities.historicbrawl ? 1 : 0,
         card.legalities.alchemy ? 1 : 0,
         card.legalities.paupercommander ? 1 : 0,
-        card.legalities.duel ? 1 : 0,,
+        card.legalities.duel ? 1 : 0,
         card.legalities.oldschool ? 1 : 0,
         card.legalities.premodern ? 1 : 0,
         card.legalities.predh ? 1 : 0,
@@ -343,16 +349,16 @@ module.exports = async () => {
     console.log("🚀 Updating cards database");
     cards = await getDirectories(cardsDir);
     const errors = [];
-    bar.start(cards.length, 0);
+    //bar.start(cards.length, 0);
     for (const dir of cards){
         try {
             await write(dir);
         } catch (error){
             errors.push(error);
         }
-        bar.increment();
+        //bar.increment();
     }
-    bar.stop();
+    //bar.stop();
     console.log("✔️  Finished importing cards");
     if (errors.length){
         console.log("🚨 Errors:");
